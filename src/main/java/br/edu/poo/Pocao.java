de757df3afd5fb5dfde308e7a5bb600e99577867
0legari0 <viniciusgomessz11@gmail.com>
2026-06-18 23:24:52 -0300

    Update Pocao.java

--- src/main/java/br/edu/poo/Pocao.java
+++ src/main/java/br/edu/poo/Pocao.java
@@ -14,3 +14,24 @@ public class Pocao {
         return 0;
     }
 }
+package br.edu.poo;
+
+public class Pocao {
+
+    private String nome;
+    private int cura;
+
+    public Pocao(String nome, int cura) {
+
+        this.nome = nome;
+        this.cura = cura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCura() {
+        return cura;
+    }
+}
