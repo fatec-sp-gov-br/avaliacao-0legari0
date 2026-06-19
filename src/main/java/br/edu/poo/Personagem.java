ba68378f6ecc9420e78a15adca8ba04caed9cb4a
0legari0 <viniciusgomessz11@gmail.com>
2026-06-18 23:24:16 -0300

    Implement Personagem class with combat methods

--- src/main/java/br/edu/poo/Personagem.java
+++ src/main/java/br/edu/poo/Personagem.java
@@ -33,3 +33,78 @@ public class Personagem {
         return 0;
     }
 }
+package br.edu.poo;
+
+public class Personagem {
+
+    private String nome;
+    private int vida;
+    private int ataque;
+    private int defesa;
+    private boolean pocaoUtilizada;
+
+    public Personagem(String nome,
+                      int vida,
+                      int ataque,
+                      int defesa) {
+
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.pocaoUtilizada = false;
+    }
+
+    public void atacar(Personagem inimigo) {
+
+        int dano = this.ataque - inimigo.defesa;
+
+        if (dano < 1) {
+            dano = 1;
+        }
+
+        inimigo.receberDano(dano);
+
+        System.out.println(nome + " ataca " + inimigo.getNome());
+        System.out.println(inimigo.getNome() +
+                " possui " + inimigo.getVida() +
+                " de vida\n");
+    }
+
+    public void receberDano(int dano) {
+
+        vida -= dano;
+
+        if (vida < 0) {
+            vida = 0;
+        }
+    }
+
+    public void usarPocao(Pocao pocao) {
+
+        if (!pocaoUtilizada) {
+
+            vida += pocao.getCura();
+            pocaoUtilizada = true;
+
+            System.out.println(nome +
+                    " usa " + pocao.getNome());
+
+            System.out.println(nome +
+                    " possui " + vida +
+                    " de vida\n");
+        }
+    }
+
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+}
