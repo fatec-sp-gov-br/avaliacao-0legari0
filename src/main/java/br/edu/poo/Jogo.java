0290b9f0e2866104161df15dbbc064971fc91993
0legari0 <viniciusgomessz11@gmail.com>
2026-06-18 23:22:07 -0300

    Implement battle logic between characters

--- src/main/java/br/edu/poo/Jogo.java
+++ src/main/java/br/edu/poo/Jogo.java
@@ -9,3 +9,49 @@ public class Jogo {
     }
 
 }
+package br.edu.poo;
+
+public class Jogo {
+
+    public static void main(String[] args) {
+
+        Personagem artemis = new Personagem(
+                "Artemis", 30, 8, 2);
+
+        Personagem goblin = new Personagem(
+                "Goblin", 20, 5, 1);
+
+        Pocao pequena = new Pocao(
+                "Poção Pequena", 10);
+
+        System.out.println("=== BATALHA INICIADA ===\n");
+
+        artemis.atacar(goblin);
+
+        goblin.atacar(artemis);
+
+        goblin.usarPocao(pequena);
+
+        while (artemis.estaVivo() &&
+               goblin.estaVivo()) {
+
+            artemis.atacar(goblin);
+
+            if (!goblin.estaVivo()) {
+                break;
+            }
+
+            goblin.atacar(artemis);
+        }
+
+        System.out.println("\n=== FIM DA BATALHA ===\n");
+
+        if (artemis.estaVivo()) {
+            System.out.println(
+                "Vencedor: " + artemis.getNome());
+        } else {
+            System.out.println(
+                "Vencedor: " + goblin.getNome());
+        }
+    }
+}
